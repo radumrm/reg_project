1f672ac3b47771d3c3d7786a772b4380b01c7861
radumrm <marincearadumihail@gmail.com>
2025-05-20 23:21:41 +0300

    Generated the data set we will use

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,1 +1,9 @@
-print("Test")
+import pandas as pd
+
+def prepare_dataset():
+    df = pd.read_csv("vienna_weekdays.csv")
+    to_remove = ['room_shared', 'multi', 'biz', 'lng', 'lat', 'attr_index', 'rest_index']
+    df = df[[col for col in df.columns if col not in to_remove]]
+    df.to_csv("dataset.csv", index=False)
+
+prepare_dataset()
