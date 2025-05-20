9e9a7e4573d093c1852f9a3f58493933f747d7f6
radumrm <marincearadumihail@gmail.com>
2025-05-21 00:36:37 +0300

    Am simulat data lipsa si adaugat zgomot

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,9 +1,22 @@
 import pandas as pd
+import numpy as np
 
 def prepare_dataset():
     df = pd.read_csv("vienna_weekdays.csv")
-    to_remove = ['room_shared', 'multi', 'biz', 'lng', 'lat', 'attr_index', 'rest_index']
+
+    # Scoatem coloanele care nu sunt relevante
+    to_remove = ['room_shared', 'multi', 'biz', 'attr_index', 'rest_index']
     df = df[[col for col in df.columns if col not in to_remove]]
-    df.to_csv("dataset.csv", index=False)
+
+    # Introdugem zgomot in realSum
+    noise = np.random.normal(loc = 0, scale = 0.05, size = len(df))
+    df['realSum'] = df['realSum'] * (noise + 1)
+
+    # Introducem valori lipsa la realSum, dist si guest_satisfaction de 5%
+    for column in ['realSum', 'guest_satisfaction_overall', 'realSum']:
+        to_Remove = np.random.choice(df.index, int(0.05 * len(df)), replace = False)
+        df.loc[to_Remove, column] = np.nan
+    
+    df.to_csv("data_set.csv", index=False)
 
 prepare_dataset()
